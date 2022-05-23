d238a4479f927734c8e9e1de9e440d7b6ab38447
ramesh kharbuja <rankerramesh@gmail.com>
2022-05-23 10:19:29 +0545

    Add project files.

---
+++ BusinessService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BMCTutorial.Models;
+
+namespace BMCTutorial
+{
+    public class BusinessService
+    {
+        public static List<PersonModel> people = new List<PersonModel>();
+    }
+}
---
+++ Controllers/PersonController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BMCTutorial.Models;
+
+namespace BMCTutorial.Controllers
+{
+    public class PersonController : Controller
+    {
+        public PersonController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            List<PersonModel> people = BusinessService.people;
+            return View(people);
+        }
+        public IActionResult Create()
+        {
+            PersonModel person = new PersonModel();
+            return View(person);
+        }
+        [HttpPost]
+        public IActionResult Create(PersonModel person)
+        {
+            List<PersonModel> people = BusinessService.people;
+            people.Add(person);
+            return RedirectToAction("Index");
+        }
+        public IActionResult EditPerson(int id)
+        {
+            List<PersonModel> people = BusinessService.people;
+            PersonModel person = people.Where(x => x.ID == id).FirstOrDefault();
+            return View(person);
+        }
+        [HttpPost]
+        public IActionResult EditPerson(PersonModel person)
+        {
+            List<PersonModel> people = BusinessService.people;
+            PersonModel p = people.Where(x => x.ID == person.ID).FirstOrDefault();
+            p.Name = person.Name;
+            p.Address = person.Address;
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            List<PersonModel> people = BusinessService.people;
+            PersonModel p = people.Where(x => x.ID == id).FirstOrDefault();
+            people.Remove(p);
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
