1f9d6dddadb958871340f55712c0bf292cc9aeee
ramesh kharbuja <rankerramesh@gmail.com>
2022-05-24 12:06:03 +0545

    new files

---
+++ Models/PersonPartial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BMCTutorial.Models
+{
+    public partial class PersonModel
+    {
+        public string MobileNo { get; set; }
+    }
+}
